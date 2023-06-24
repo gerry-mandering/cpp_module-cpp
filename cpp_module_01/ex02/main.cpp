9b9b79ede6a9429c8a2904b144051a7a494aa79e
gerry-mandering <kimmen1119@gmail.com>
2023-06-24 13:55:13 +0900

    Feat: cpp01 ex02

--- cpp_module_01/ex02/main.cpp
+++ cpp_module_01/ex02/main.cpp
@@ -9,3 +9,21 @@
 /*   Updated: 2023/06/23 22:53:35 by minseok2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+
+int main(void) {
+    std::string greetings("HI THIS IS BRAIN");
+    std::string *stringPTR = &greetings;
+    std::string &stringREF = greetings;
+
+    std::cout << "The Memory Address of greetings: " << &greetings << std::endl;
+    std::cout << "The Memory Address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "The Memory Address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "The Value of greetings: " << greetings << std::endl;
+    std::cout << "The Value Pointed by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The Value Pointed by stringREF: " << stringREF << std::endl;
+
+    return 0;
+}
