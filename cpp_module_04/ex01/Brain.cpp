1bd984a9fa99f60205b792fc1cdc6e72e97199c8
gerry-mandering <kimmen1119@gmail.com>
2023-07-09 16:34:02 +0900

    Feat: making CPP04 ex01 Brain

--- cpp_module_04/ex01/Brain.cpp
+++ cpp_module_04/ex01/Brain.cpp
@@ -9,3 +9,35 @@
 /*   Updated: 2023/07/03 13:25:03 by minseok2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Brain.hpp"
+
+Brain::Brain() {
+    std::cout << "Brain Constructor called" << std::endl;
+
+    for (int i = 0; i < Brain::NUMBER_OF_IDEAS; i++) {
+        std::stringstream stream;
+
+        stream << "idea" << (i + 1);
+        ideas[i] = stream.str();
+    }
+}
+
+Brain::~Brain() {
+    std::cout << "Brain Destructor called" << std::endl;
+}
+
+Brain::Brain(const Brain &brain) {
+    *this = brain;
+}
+
+Brain &Brain::operator=(const Brain &brain) {
+    if (this == &brain)
+        return *this;
+
+    for (int i = 0; i < Brain::NUMBER_OF_IDEAS; i++) {
+        ideas[i] = brain.ideas[i];
+    }
+
+    return *this;
+}
--- cpp_module_04/ex01/Brain.hpp
+++ cpp_module_04/ex01/Brain.hpp
@@ -9,3 +9,25 @@
 /*   Updated: 2023/07/03 13:25:03 by minseok2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef BRAIN_H
+#define BRAIN_H
+
+#include <iostream>
+#include <sstream>
+
+class Brain {
+public:
+    Brain();
+    ~Brain();
+    Brain(const Brain &brain);
+    Brain &operator=(const Brain &brain);
+
+private:
+    static const unsigned int NUMBER_OF_IDEAS = 100;
+
+private:
+    std::string ideas[Brain::NUMBER_OF_IDEAS];
+};
+
+#endif
